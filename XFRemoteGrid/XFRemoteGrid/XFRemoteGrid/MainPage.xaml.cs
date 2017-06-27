a32546d9278494a169a5c077469a379ba10821a2
nellytacheva <nellytacheva@users.noreply.github.com>
2017-06-27 10:55:19 -0400

    How to Connect XamDataGrid to a Virtual Data Source
    Sample app for blog post "How to Connect XamDataGrid to a Virtual Data Source".

---
+++ XFRemoteGrid/XFRemoteGrid/XFRemoteGrid/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Infragistics.Controls.DataSource;
+using Xamarin.Forms;
+
+namespace XFRemoteGrid
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+
+			var dataSource = new ODataVirtualDataSource()
+			{
+				BaseUri = "http://services.odata.org/V4/Northwind/Northwind.svc",
+				EntitySet = "Orders",
+				PageSizeRequested = 25,
+				MaxCachedPages = 5
+			};
+
+			this.BindingContext = dataSource;
+		}
+	}
+}
